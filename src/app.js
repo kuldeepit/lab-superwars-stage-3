898518e5e498c43b544c32ef867d39cd36b8879b
kuldeepit <kuldeepit108@gmail.com>
2024-02-29 18:22:48 +0530

    made it funcitonal using different methods

--- src/app.js
+++ src/app.js
@@ -18,42 +18,86 @@ const PLAYERS = [
     "Thor",
     "Slayer",
     "Vader",
-    "Slingo"
-];
-
-// initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
-
+    "Slingo",
+  ];
+  
+  // initialize players with image and strength
+  // Instead of forloop use Map method
+  // Code here
+  const initPlayers = (players) => {
+    let detailedPlayers = players.map(function (players, i) {
+      if (i % 2 === 0) {
+        var type = "hero";
+      } else {
+        var type = "villain";
+      }
+      const details = {
+        name: players,
+        strength: getRandomStrength(),
+        image: `images/super-${i + 1}.png`,
+        type: type,
+      };
+      return details;
+    });
     return detailedPlayers;
-}
-
-// getting random strength
-const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-}
-
-// Build player template
-const buildPlayers = (players, type) => {
-    let fragment = '';
-
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
-
+  };
+  
+  // getting random strength
+  const getRandomStrength = () => {
+    return Math.floor(Math.random() * 100) + 1;
+  };
+  
+  // Build player template
+  const buildPlayers = (players, type) => {
+    let fragment = "";
+    // filter players starts from here
+    const hero = players.filter(function (obj) {
+      return obj.type === "hero";
+    });
+    const villain = players.filter(function (obj) {
+      return obj.type === "villain";
+    });
+    // filter players ends from here
+  
+    if (type === "hero") {
+      // creating a innerHTML array of each player
+      var x = hero.map(function (players) {
+        let frag = `<div class="player">
+        <img src="${players.image}" alt="">
+        <div class="name">${players.name}</div>
+        <div class="strength">${players.strength}</div>
+      </div>`;
+        // returning each innerHTML to frag
+        return frag;
+      });
+      // Joining all the elements in frag into string
+      fragment = x.join(" ");
+    } else {
+      // creating a innerHTML array of each player
+      var x = villain.map(function (players) {
+        let frag = `<div class="player">
+        <img src="${players.image}" alt="">
+        <div class="name">${players.name}</div>
+        <div class="strength">${players.strength}</div>
+      </div>`;
+        // returning each innerHTML to frag
+        return frag;
+      });
+      // Joining all the elements in frag into string
+      fragment = x.join(" ");
+    }
     return fragment;
-}
-
-// Display players in HTML
-const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
-
-window.onload = () => {
+  };
+  
+  // Display players in HTML
+  const viewPlayers = (players) => {
+    document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+    document.getElementById("villains").innerHTML = buildPlayers(
+      players,
+      "villain"
+    );
+  };
+  
+  window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
-}
+  };
